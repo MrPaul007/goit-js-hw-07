96259e37a379d3ca13172e13c30d1186678242f5
MrPaul007 <75275133+MrPaul007@users.noreply.github.com>
2021-03-03 17:33:53 +0200

    fixed 7-3 and 7-1

--- js/task-7_1.js
+++ js/task-7_1.js
@@ -1,8 +1,6 @@
 const CategoriesElem = document.querySelectorAll(".item");
 console.log(`В списке ${CategoriesElem.length} категории.`);
 
-const itemElem = document.querySelectorAll(".item");
-
 const category = (array) => {
     array.forEach((element) => {
         const title = element.querySelector("h2");
@@ -11,4 +9,4 @@ const category = (array) => {
         Количество элементов: ${itemLi.length}`);
     });
 };
-category(itemElem);
+category(CategoriesElem);
